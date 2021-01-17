5439bfddf2fb6389af73b19e3fd46432902ca76f
cucearda <cuce.arda99@gmail.com>
2021-01-17 22:07:52 +0300

    creat fonku silindi

--- app.py
+++ app.py
@@ -9,53 +9,51 @@ import user
 from flask_login import LoginManager
 
 
-def create_app():
-    conn = psycopg2.connect(dbname= "recipeas2", user="postgres", host='localhost', password= "arda")
-    cur = conn.cursor()
-    lm = LoginManager()
 
-    @lm.user_loader
-    def load_user(username):
-        return user.get_user(username)
+conn = psycopg2.connect(dbname= "recipeas2", user="postgres", host='localhost', password= "arda")
+cur = conn.cursor()
+lm = LoginManager()
 
-    app = Flask(__name__)
-    app.add_url_rule("/", view_func=view.home_page)
-    app.add_url_rule("/recipes", view_func=view.recipes_page,  methods = ["GET", "POST"])
-    app.add_url_rule("/recipes/<int:recipe_id>", view_func = view.recipe_page)
-    app.add_url_rule("/posts", view_func= view.posts_page, methods = ["GET", "POST"])
-    app.add_url_rule("/post/<int:post_id>", view_func = view.post_page, methods = ["GET", "POST"])
-    app.add_url_rule("/post?<int:post_id>?<int:comment_id>", view_func = view.delete_comment_page)
-    app.add_url_rule("/create_post", view_func= view.create_post_page, methods =["GET", "POST"])
-    app.add_url_rule("/create_recipe", view_func = view.create_recipe_page, methods =["GET", "POST"])
-    app.add_url_rule("/signup", view_func=view.signup_page, methods = ["POST","GET"])
-    app.add_url_rule("/login", view_func=view.login_page, methods=["POST", "GET"])
-    app.add_url_rule("/logout", view_func=view.logout_page)
-    app.add_url_rule("/create_ingredient", view_func=view.create_ingredient_page, methods=["POST", "GET"])
-    app.add_url_rule("/create_tool", view_func= view.create_tool_page, methods = ["POST", "GET"])
-    app.add_url_rule("/tried/<int:recipe_id>", view_func = view.tried_page)
-    app.add_url_rule("/recipes/<int:recipe_id>/<int:vote_type>", view_func = view.vote_recipe_page)
-    app.add_url_rule("/post/<int:post_id>/<int:vote_type>", view_func=view.vote_post_page)
-    app.add_url_rule("/post/<int:comment_id>+<int:post_id>/<int:vote_type>", view_func=view.vote_comment_page)
-    app.add_url_rule("/search_recipe", view_func=view.search_recipe_page, methods = ["POST", "GET"])
-    app.add_url_rule("/top_recipes", view_func=view.top_recipes_page, methods = ["POST", "GET"])
-    app.add_url_rule("/user_rankings", view_func=view.user_rankings_page, methods = ["POST", "GET"])
-    
-    db = Database()
-    app.config["db"] = db
+@lm.user_loader
+def load_user(username):
+    return user.get_user(username)
 
-    lm.init_app(app)
-    lm.login_view = "login_page"
+app = Flask(__name__)
+app.add_url_rule("/", view_func=view.home_page)
+app.add_url_rule("/recipes", view_func=view.recipes_page,  methods = ["GET", "POST"])
+app.add_url_rule("/recipes/<int:recipe_id>", view_func = view.recipe_page)
+app.add_url_rule("/posts", view_func= view.posts_page, methods = ["GET", "POST"])
+app.add_url_rule("/post/<int:post_id>", view_func = view.post_page, methods = ["GET", "POST"])
+app.add_url_rule("/post?<int:post_id>?<int:comment_id>", view_func = view.delete_comment_page)
+app.add_url_rule("/create_post", view_func= view.create_post_page, methods =["GET", "POST"])
+app.add_url_rule("/create_recipe", view_func = view.create_recipe_page, methods =["GET", "POST"])
+app.add_url_rule("/signup", view_func=view.signup_page, methods = ["POST","GET"])
+app.add_url_rule("/login", view_func=view.login_page, methods=["POST", "GET"])
+app.add_url_rule("/logout", view_func=view.logout_page)
+app.add_url_rule("/create_ingredient", view_func=view.create_ingredient_page, methods=["POST", "GET"])
+app.add_url_rule("/create_tool", view_func= view.create_tool_page, methods = ["POST", "GET"])
+app.add_url_rule("/tried/<int:recipe_id>", view_func = view.tried_page)
+app.add_url_rule("/recipes/<int:recipe_id>/<int:vote_type>", view_func = view.vote_recipe_page)
+app.add_url_rule("/post/<int:post_id>/<int:vote_type>", view_func=view.vote_post_page)
+app.add_url_rule("/post/<int:comment_id>+<int:post_id>/<int:vote_type>", view_func=view.vote_comment_page)
+app.add_url_rule("/search_recipe", view_func=view.search_recipe_page, methods = ["POST", "GET"])
+app.add_url_rule("/top_recipes", view_func=view.top_recipes_page, methods = ["POST", "GET"])
+app.add_url_rule("/user_rankings", view_func=view.user_rankings_page, methods = ["POST", "GET"])
+
+db = Database()
+app.config["db"] = db
+
+lm.init_app(app)
+lm.login_view = "login_page"
 
 
-    cur.close()
-    conn.close()
-    
-    return app
+cur.close()
+conn.close()
+
 
 
 
 if __name__ == "__main__":
-    app = create_app()
     app.secret_key = 'super secret key'
     app.debug = True
     app.run(host="0.0.0.0", port=8081, debug=True)
