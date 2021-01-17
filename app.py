fe29f4d0e3b9d2c30dfd41b06b310805f7dda228
cucearda <cuce.arda99@gmail.com>
2021-01-17 22:52:08 +0300

    fonkntan çıkart

--- app.py
+++ app.py
@@ -48,7 +48,6 @@ lm.login_view = "login_page"
 
 
 if __name__ == "__main__":
-    app = create_app()
     app.secret_key = 'super secret key'
     app.debug = True
     app.run(host="0.0.0.0", port=8081, debug=True)
